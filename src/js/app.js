983637f2d9ee5f7d8265b642b52f91401608362a
Oscar Del Olmo Lopez <odelolmo@uoc.edu>
2026-03-29 18:59:32 +0200

    Inicialización del proyecto y estructura base

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init({
+    duration: 800,
+    once: true
+  });
+
+  initMobileMenu();
+  initCategoryFilter();
+});
